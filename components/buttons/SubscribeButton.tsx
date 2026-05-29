8c814aac3fc447ad65a271d065b5ff07c98f1ced
hasithasandunlakshan <hasiofficial2002@gmail.com>
2026-05-29 18:02:46 +0530

    refactor: rename GoogleCalendarButton to SubscribeButton and update styling and icon configuration

--- components/buttons/SubscribeButton.tsx
+++ components/buttons/SubscribeButton.tsx
@@ -7,34 +7,34 @@ import { useTranslation } from '../../utils/i18n';
 import IconSubscribe from '../icons/Subscribe';
 import Button from './Button';
 
-interface IGoogleCalendarButtonProps extends IButtonDefaultProps {}
+interface ISubscribeButtonProps extends IButtonDefaultProps {}
 
 /**
- * @description The GoogleCalendarButton component is a button that links to the Google Calendar.
+ * @description The SubscribeButton component is a button that links to the meeting subscription or community events.
  * @param {string} props.text - The text to display on the button.
  * @param {string} props.href - The href attribute for the anchor tag.
  * @param {string} props.target - The target attribute for the anchor tag.
  * @param {ButtonIconPosition} props.iconPosition - The position of the icon in the button.
  * @param {string} props.className - The class name to be applied to the button.
  */
-export default function GoogleCalendarButton({
+export default function SubscribeButton({
   text = 'subscribeBtn',
   href,
   target = '_blank',
-  iconPosition = ButtonIconPosition.LEFT,
+  iconPosition = ButtonIconPosition.RIGHT,
   className
-}: IGoogleCalendarButtonProps) {
+}: ISubscribeButtonProps) {
   const { t } = useTranslation('common');
 
   return (
     <Button
       text={t(text)}
-      icon={<IconSubscribe />}
+      icon={<IconSubscribe className='size-5 transition-transform duration-300 group-hover:rotate-12' />}
       href={href}
       iconPosition={iconPosition}
       target={target}
-      className={`mt-2 block text-center text-gray-900 md:mt-0 md:inline-block ${className}`}
-      bgClassName='bg-gray-200 hover:bg-gray-100'
+      className={`group mt-2 flex items-center justify-center text-center text-white md:mt-0 md:inline-flex ${className}`}
+      bgClassName='bg-orange-500 hover:bg-orange-600 transition-all duration-300'
     />
   );
 }
