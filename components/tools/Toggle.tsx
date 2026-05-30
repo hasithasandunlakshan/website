049082639d2fac6c163414b9e3bdcd52fb3010ec
hasithasandunlakshan <hasiofficial2002@gmail.com>
2026-05-30 18:53:25 +0530

    style: refactor code formatting and consistency in Toggle and FiltersDropdown components

--- components/tools/Toggle.tsx
+++ components/tools/Toggle.tsx
@@ -11,14 +11,14 @@ const Toggle = ({
   setChecked,
   label,
   bgColor = 'bg-gray-200',
-  checkedStateBgColor = 'bg-secondary-500'
+  checkedStateBgColor = 'bg-secondary-500',
 }: ToggleProps) => {
   return (
-    <label className='relative inline-flex cursor-pointer items-center'>
+    <label className="relative inline-flex cursor-pointer items-center">
       <input
-        type='checkbox'
+        type="checkbox"
         value={checked ? 'true' : 'false'}
-        className='peer sr-only'
+        className="peer sr-only"
         onChange={() => setChecked(!checked)}
       />
       <div
@@ -29,10 +29,15 @@ const Toggle = ({
           after:border-gray-300 after:bg-white after:transition-all after:content-[''] dark:after:border-gray-500
           dark:after:bg-gray-200`,
           bgColor,
-          checked && `after:translate-x-full after:border-white ${checkedStateBgColor} dark:bg-secondary-600 dark:ring-secondary-400/60`
+          checked &&
+            `after:translate-x-full after:border-white ${checkedStateBgColor} dark:bg-secondary-600 dark:ring-secondary-400/60`,
         )}
       ></div>
-      {label && <div className='ml-2 text-sm font-medium text-gray-700 dark:text-gray-300'>{label}</div>}
+      {label && (
+        <div className="ml-2 text-sm font-medium text-gray-700 dark:text-gray-300">
+          {label}
+        </div>
+      )}
     </label>
   );
 };
